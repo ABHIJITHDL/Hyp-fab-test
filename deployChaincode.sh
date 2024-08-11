88dd183f685d9a8d7b6dc4a8ffef507cefe5bb84
ABHIJITHDL <abhijith20034@gmail.com>
2024-08-11 09:20:21 +0530

    Car owner updation working

--- deployChaincode.sh
+++ deployChaincode.sh
@@ -14,7 +14,7 @@ setGlobalsForOrderer(){
 }
 
 setGlobalsForPeer0Org1(){
-    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_LOCALMSPID=Org1MSP
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
     # export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp
@@ -22,7 +22,7 @@ setGlobalsForPeer0Org1(){
 }
 
 setGlobalsForPeer1Org1(){
-    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_LOCALMSPID=Org1MSP
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
     export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
     export CORE_PEER_ADDRESS=localhost:8051
